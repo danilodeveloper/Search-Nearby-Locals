b690e602fec6fab358a5b99bdc3d57e95befa891
Bruno Soares <brunossn@gmail.com>
2013-08-09 11:00:47 -0300

    Convert application to a Rails Application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geo::Application.config.secret_token = 'a72dc1161f1d45e59efb1dcae3d1d8ad6e69d5332e87a5ceff52ee4bbb0a73a7bccb98d78ed57d893eaa3be611814fbc4d66a99432bb2cf14f9d5b3b940474d5'
